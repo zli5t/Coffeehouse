fc18455d93b2d9aea29de46a651e398e6d85c8c8
Semon Rezchikov <semonr@gmail.com>
2012-01-16 19:34:16 -0500

    testing templates

--- web.py
+++ web.py
@@ -1,10 +1,11 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/<name>")
+def hello(name=None):
+    return render_template("hello.html", name=name)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
